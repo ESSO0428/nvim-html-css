2b5e56129dae6922517a2d0d9f5252ee35a2d5ed
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2024-02-14 02:40:00 +0800

    Update embedded.lua

--- lua/html-css/embedded.lua
+++ lua/html-css/embedded.lua
@@ -20,6 +20,12 @@ local unique_ids = {}
 local unique_ids_rule_set = {}
 
 -- treesitter query for extracting css clasess
+local htmldjango_extends_qs = [[
+  (unpaired_statement
+   (tag_name)@tag_name
+   (string)@string)
+]]
+
 local style_qs = [[
   (style_element
     (raw_text)@style_content)
@@ -35,6 +41,34 @@ local qs = [[
 	(class_selector
 		(class_name)@class_name)
 ]]
+
+local function get_extends_template(data, htmldjango_extends_qs)
+  local parser = ts.get_string_parser(data, "htmldjango")
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  local query = ts.query.parse("htmldjango", htmldjango_extends_qs)
+  local tb_extend_links = {}
+
+  -- Traverse the matched {% extends 'template.html' %}
+  local last_tag_name = ''
+  -- use below query when current filetype is htmldjango
+  if vim.bo.filetype == "htmldjango" then
+    for id, node in query:iter_captures(root, data, 0, -1) do
+      if node:type() == "tag_name" then
+        local tag_name = ts.get_node_text(node, data)
+        last_tag_name = tag_name
+      end
+      if node:type() == "string" then
+        if last_tag_name == "extends" then
+          local extend_link = ts.get_node_text(node, data)
+          extend_link = extend_link:gsub('^["\']', ''):gsub('["\']$', '')
+          table.insert(tb_extend_links, extend_link)
+        end
+      end
+    end
+  end
+  return tb_extend_links
+end
 
 local function extract_styles(data, style_query, root)
   local tb_styles = {}
@@ -99,6 +133,15 @@ local function get_current_buffer_content_as_string()
   return file, data
 end
 
+local function read_file(file)
+  local fd, err = io.open(file, "r")
+  if not fd then return nil, err end -- 如果打开文件失败，返回nil和错误信息
+
+  local data = fd:read("*a")         -- 尝试读取文件内容
+  fd:close()
+  return data
+end
+
 -- TODO change name of the function to something better
 M.read_html_files = a.wrap(function(cb)
   -- clean tables to avoid duplications
@@ -108,32 +151,60 @@ M.read_html_files = a.wrap(function(cb)
   local file, data = get_current_buffer_content_as_string()
   local file_name = u.get_file_name(file, "[^/]+$")
   file_name = table.concat({ "", file_name }, " ")
-  local styles = extract_styles_from_html(data)
-  local rule_sets = extract_rule_sets(styles, rule_set_qs)
+
+  local tb_extend_links = get_extends_template(data, htmldjango_extends_qs)
+  local files = {}
+  files = {
+    { file_name, data }
+  }
+
+  -- FIX: have some bug and so slow
+  -- for _, v in ipairs(tb_extend_links) do
+  --   table.insert(files, v)
+  -- end
+
   unique_class = {}
   unique_class_rule_set = {}
   unique_ids = {}
   unique_ids_rule_set = {}
 
-  for _, rule in ipairs(rule_sets) do
-    local parser = ts.get_string_parser(rule, "css")
-    local tree = parser:parse()[1]
-    local root = tree:root()
-    local query = ts.query.parse("css", qs)
+  for _, file in ipairs(files) do
+    local status = true
+    if _ == 1 then
+      local file_name = file[1]
+      local data = file[2]
+    else
+      local current_file_path = vim.fn.expand('%:p:h')
+      local file = current_file_path .. "/" .. file
+      local file_name = u.get_file_name(file, "[^/]+$")
+      local data
+      data = pcall(read_file, file)
+    end
+    if status then
+      local styles = extract_styles_from_html(data)
+      local rule_sets = extract_rule_sets(styles, rule_set_qs)
 
-    for _, matches, _ in query:iter_matches(root, rule, 0, 0, {}) do
-      local last_chid_node = ''
-      for _, node in pairs(matches) do
-        if node:type() == "id_name" then
-          last_chid_node = node:type()
-          local id_name = ts.get_node_text(node, rule)
-          table.insert(unique_ids, id_name)
-          table.insert(unique_ids_rule_set, rule)
-        elseif node:type() == "class_name" then
-          last_chid_node = node:type()
-          local class_name = ts.get_node_text(node, rule)
-          table.insert(unique_class, class_name)
-          table.insert(unique_class_rule_set, rule)
+      for _, rule in ipairs(rule_sets) do
+        local parser = ts.get_string_parser(rule, "css")
+        local tree = parser:parse()[1]
+        local root = tree:root()
+        local query = ts.query.parse("css", qs)
+
+        for _, matches, _ in query:iter_matches(root, rule, 0, 0, {}) do
+          local last_chid_node = ''
+          for _, node in pairs(matches) do
+            if node:type() == "id_name" then
+              last_chid_node = node:type()
+              local id_name = ts.get_node_text(node, rule)
+              table.insert(unique_ids, id_name)
+              table.insert(unique_ids_rule_set, rule)
+            elseif node:type() == "class_name" then
+              last_chid_node = node:type()
+              local class_name = ts.get_node_text(node, rule)
+              table.insert(unique_class, class_name)
+              table.insert(unique_class_rule_set, rule)
+            end
+          end
         end
       end
     end
