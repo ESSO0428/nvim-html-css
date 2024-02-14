138c3db47ea0544d798bf2584798039e089dfffa
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2024-02-15 04:39:21 +0800

    Update init.lua

--- lua/html-css/init.lua
+++ lua/html-css/init.lua
@@ -17,6 +17,31 @@ local function mrgtbls(t1, t2)
   return t1
 end
 
+local function is_link_tag_changed()
+  -- 這裡使用 Treesitter 查詢來檢查當前是否有 link 標籤的更改
+  local parser = ts.get_parser(0, 'html')
+  local tree = parser:parse()[1]
+  local root = tree:root()
+  local query = ts.query.parse('html', [[
+    (element
+      (start_tag
+        (tag_name) @tag
+        (attribute
+          (attribute_name) @name
+          (quoted_attribute_value) @value))
+      (text) @text)
+  ]])
+
+  for id, node in query:iter_captures(root, 0, 0, -1) do
+    local tag = vim.treesitter.get_node_text(node, 0)
+    if tag == "link" then
+      return true
+    end
+  end
+  return false
+end
+
+
 source.new = function()
   local self = setmetatable({}, { __index = source })
   self.source_name = "html-css"
@@ -25,10 +50,13 @@ source.new = function()
   self.items = {}
   self.ids = {}
   self.href_links = {}
+  self.after_inert_before_update = false
 
   self.embedded = ''
   self.remote = ''
+  self.remote_item_write = ''
   self.local_file = ''
+  self.update_done = ''
 
   -- reading user config
   self.user_config = config.get_source_config(self.source_name) or {}
@@ -37,90 +65,180 @@ source.new = function()
   self.style_sheets = self.option.style_sheets or {}
   self.enable_on = self.option.enable_on or {}
 
-  self.href_links = h.get_hrefs()
+  -- self.href_links = h.get_hrefs()
 
   -- merge lings together
-  self.style_sheets = mrgtbls(self.style_sheets, self.href_links)
+  -- self.style_sheets = mrgtbls(self.style_sheets, self.href_links)
 
   -- set autocmd to update completion data when file is opened
-  local augroup = vim.api.nvim_create_augroup('HTMLCSSCompletion', { clear = true })
-  vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
+  local augroup = vim.api.nvim_create_augroup('HTMLCSSCompletionForceUpdate', { clear = true })
+  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
     group = augroup,
     pattern = { '*.html' },
     callback = function()
-      self:update_completion_data()
-    end,
+      while not self.update_done == 'update' do
+        a.wait(50)
+      end
+      if self.update_done == '' or self.update_done == 'done' then
+        self.after_inert_before_update = true
+        self:update_completion_data('force')
+      end
+    end
+  })
+  local augroup = vim.api.nvim_create_augroup('HTMLCSSCompletionTextChange', { clear = true })
+  vim.api.nvim_create_autocmd({ 'InsertEnter', 'InsertLeave' }, {
+    group = augroup,
+    pattern = { '*.html' },
+    callback = function()
+      if self.update_done == '' or self.update_done == 'done' then
+        local inside_quotes = ts.get_node({ bfnr = 0, lang = 'html' })
+
+        if inside_quotes ~= nil then
+          local type = inside_quotes:type()
+
+          local prev_sibling = inside_quotes:prev_named_sibling()
+          if prev_sibling ~= nil then
+            local prev_sibling_name = ts.get_node_text(prev_sibling, 0)
+
+            if prev_sibling_name == "href" then
+              self.after_inert_before_update = true
+            end
+          end
+        end
+      end
+    end
+  })
+  local augroup = vim.api.nvim_create_augroup('HTMLCSSCompletionLinkChange', { clear = true })
+  vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+    group = augroup,
+    pattern = { '*.html' },
+    callback = function()
+      -- if have a function to check if the current edited node is a link tag
+      if is_link_tag_changed() then
+        self.after_inert_before_update = true
+      end
+    end
+  })
+
+
+  local augroup = vim.api.nvim_create_augroup('HTMLCSSCompletionConditionUpdate', { clear = true })
+  vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+    group = augroup,
+    pattern = { '*.html' },
+    callback = function()
+      if self.update_done == '' or self.update_done == 'done' then
+        self:update_completion_data('condition')
+      end
+    end
   })
   return self
 end
 
-function source:update_completion_data()
-  self.remote_classes = {}
-  self.items = {}
-  self.ids = {}
-  self.href_links = h.get_hrefs()
-  self.style_sheets = self.option.style_sheets or {}
+function source:check_update_completion()
+  if self.embedded and self.remote and self.remote_item_write and self.local_file then
+    self.after_inert_before_update = false
+    self.update_done = 'done'
+    print("html-css completion preloading done!")
+  end
+end
 
-  self.embedded = ''
-  self.remote = ''
-  self.local_file = ''
-
-  -- merge links together
-  self.style_sheets = mrgtbls(self.style_sheets, self.href_links)
-
-  -- handle embedded styles
-  a.run(function()
-    e.read_html_files(function(classes, ids, links)
-      for _, class in ipairs(classes) do
-        table.insert(self.items, class)
+function source:update_completion_data(group_type)
+  if group_type ~= 'condition' or self.after_inert_before_update then
+    -- Reset flag immediately to prevent multiple triggers
+    vim.defer_fn(function()
+      print("Load html-css completion data ...")
+      if not vim.tbl_contains(self.option.enable_on, vim.bo.filetype) then
+        return
       end
-      for _, id in ipairs(ids) do
-        table.insert(self.ids, id)
+      if group_type == 'condition' and self.after_inert_before_update == false then
+        return
       end
-      self.style_sheets = mrgtbls(self.style_sheets, links)
-      self.style_sheets = u.unique_list(self.style_sheets)
-    end)
-    self.embedded = 'done'
-  end)
+      if self.update_done == '' or self.update_done == 'done' then
+        self.embedded = ''
+        self.remote = ''
+        self.remote_item_write = ''
+        self.local_file = ''
+        self.remote_classes = {}
+        self.items = {}
+        self.ids = {}
+      end
+      self.update_done = 'update'
 
-  -- Remote css reading
-  a.run(function()
-    if self.embedded == 'done' then
-      for _, url in ipairs(self.style_sheets) do
-        if url:match(self.isRemote) then
-          a.run(function()
-            r.init(url, function(classes)
-              for _, class in ipairs(classes) do
-                table.insert(self.remote_classes, class)
-              end
+      a.run(function()
+        -- if self.embedded == 'done' and self.update_done == 'update' then
+        --   return
+        -- end
+        -- merge links together
+        self.href_links = h.get_hrefs()
+        self.style_sheets = self.option.style_sheets or {}
+        self.style_sheets = mrgtbls(self.style_sheets, self.href_links)
+        -- handle embedded styles
+        e.read_html_files(function(classes, ids, links)
+          for _, class in ipairs(classes) do
+            table.insert(self.items, class)
+          end
+          for _, id in ipairs(ids) do
+            table.insert(self.ids, id)
+          end
+          self.style_sheets = mrgtbls(self.style_sheets, links)
+          self.style_sheets = u.unique_list(self.style_sheets)
+          self.embedded = 'done'
+          self:check_update_completion()
+        end)
+      end)
+
+      -- Remote css reading
+      a.run(function()
+        -- if self.remote == 'done' and self.update_done == 'update' then
+        --   return
+        -- end
+        for _, url in ipairs(self.style_sheets) do
+          if url:match(self.isRemote) then
+            a.run(function()
+              r.init(url, function(classes)
+                for _, class in ipairs(classes) do
+                  table.insert(self.remote_classes, class)
+                end
+              end)
             end)
-          end)
+          end
         end
         self.remote = 'done'
-      end
-    end
-  end)
+        self:check_update_completion()
+      end)
 
-  -- Local css reading
-  a.run(function()
-    l.read_local_files(self.href_links, function(classes, ids)
-      for _, class in ipairs(classes) do
-        table.insert(self.items, class)
-      end
-      for _, id in ipairs(ids) do
-        table.insert(self.ids, id)
-      end
-
-      for _, class in ipairs(self.remote_classes) do
-        table.insert(self.items, class)
-      end
-    end)
-    self.local_file = 'done'
-  end)
+      -- Local css reading
+      a.run(function()
+        -- if self.local_file == 'done' and self.update_done == 'update' then
+        --   return
+        -- end
+        self.local_file = ''
+        l.read_local_files(self.style_sheets, function(classes, ids)
+          for _, class in ipairs(classes) do
+            table.insert(self.items, class)
+          end
+          for _, id in ipairs(ids) do
+            table.insert(self.ids, id)
+          end
+        end)
+        self.local_file = 'done'
+      end)
+      a.run(function()
+        -- if self.remote_item_write == 'done' or self.remote ~= 'done' then
+        --   return
+        -- end
+        for _, class in ipairs(self.remote_classes) do
+          table.insert(self.items, class)
+        end
+        self.remote_item_write = 'done'
+        self:check_update_completion()
+      end)
+    end, 5000)
+  end
 end
 
 function source:complete(_, callback)
-  if self.embedded == 'done' and self.remote == 'done' and self.local_file == 'done' then
+  if self.update_done == 'done' then
     if self.current_selector == "class" then
       callback({ items = self.items, isComplete = false })
     elseif self.current_selector == "id" then
