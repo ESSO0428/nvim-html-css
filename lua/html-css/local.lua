274c4ecf05a87b286d726dd7b5bbf5c1410b35af
ESSO0428 <92996726+ESSO0428@users.noreply.github.com>
2024-02-19 01:05:31 +0800

    support yield control to other async tasks

--- lua/html-css/local.lua
+++ lua/html-css/local.lua
@@ -9,12 +9,6 @@ local ts = vim.treesitter
 local classes = {}
 local ids = {}
 
----@type string[]
-local unique_class = {}
-local unique_class_rule_set = {}
-local unique_ids = {}
-local unique_ids_rule_set = {}
-
 -- treesitter query for extracting css clasess
 local rule_set_qs = [[
   (rule_set) @rule
@@ -55,6 +49,7 @@ end
 ---@async
 M.read_local_files = a.wrap(function(hrefs, cb)
   local files = {}
+  -- print(vim.inspect(hrefs))
   for _, href in ipairs(hrefs) do
     if not href:match("^http") then
       local status, err = pcall(function()
@@ -89,94 +84,97 @@ M.read_local_files = a.wrap(function(hrefs, cb)
     end
   end
   if #files == 0 then
-    return {}, {}
+    cb({}, {})
   else
-    classes = {} -- clean up prev classes
-    ids = {}
+    a.run(function()
+      classes = {} -- clean up prev classes
+      ids = {}
+      for _, file in ipairs(files) do
+        ---@type string
+        local file_name = u.get_file_name(file, "[^/]+$")
 
-    for _, file in ipairs(files) do
-      ---@type string
-      local file_name = u.get_file_name(file, "[^/]+$")
+        local fd = io.open(file, "r")
+        local data = fd:read("*a")
+        fd:close()
 
-      local fd = io.open(file, "r")
-      local data = fd:read("*a")
-      fd:close()
+        -- reading html files
+        -- local _, fd = a.uv.fs_open(file, "r", 438)
+        -- local _, stat = a.uv.fs_fstat(fd)
+        -- local _, data = a.uv.fs_read(fd, stat.size, 0)
+        -- a.uv.fs_close(fd)
 
-      -- reading html files
-      -- local _, fd = a.uv.fs_open(file, "r", 438)
-      -- local _, stat = a.uv.fs_fstat(fd)
-      -- local _, data = a.uv.fs_read(fd, stat.size, 0)
-      -- a.uv.fs_close(fd)
+        local rule_sets = extract_rule_sets(data, rule_set_qs)
 
-      local rule_sets = extract_rule_sets(data, rule_set_qs)
+        ---@type string[]
+        local unique_class = {}
+        local unique_class_rule_set = {}
+        local unique_ids = {}
+        local unique_ids_rule_set = {}
 
-      unique_class = {}
-      unique_class_rule_set = {}
-      unique_ids = {}
-      unique_ids_rule_set = {}
+        for _, rule in ipairs(rule_sets) do
+          a.util.scheduler()
+          local parser = ts.get_string_parser(rule, "css")
+          local tree = parser:parse()[1]
+          local root = tree:root()
+          local query = ts.query.parse("css", qs)
 
-      for _, rule in ipairs(rule_sets) do
-        local parser = ts.get_string_parser(rule, "css")
-        local tree = parser:parse()[1]
-        local root = tree:root()
-        local query = ts.query.parse("css", qs)
-
-        for _, matches, _ in query:iter_matches(root, rule, 0, 0, {}) do
-          local last_chid_node = ''
-          for _, node in pairs(matches) do
-            if node:type() == "id_name" then
-              last_chid_node = node:type()
-              local id_name = ts.get_node_text(node, rule)
-              table.insert(unique_ids, id_name)
-              table.insert(unique_ids_rule_set, rule)
-            elseif node:type() == "class_name" then
-              last_chid_node = node:type()
-              local class_name = ts.get_node_text(node, rule)
-              table.insert(unique_class, class_name)
-              table.insert(unique_class_rule_set, rule)
+          for _, matches, _ in query:iter_matches(root, rule, 0, 0, {}) do
+            local last_chid_node = ''
+            for _, node in pairs(matches) do
+              if node:type() == "id_name" then
+                last_chid_node = node:type()
+                local id_name = ts.get_node_text(node, rule)
+                table.insert(unique_ids, id_name)
+                table.insert(unique_ids_rule_set, rule)
+              elseif node:type() == "class_name" then
+                last_chid_node = node:type()
+                local class_name = ts.get_node_text(node, rule)
+                table.insert(unique_class, class_name)
+                table.insert(unique_class_rule_set, rule)
+              end
             end
           end
         end
-      end
 
-      -- local unique_list = u.unique_list(unique_class)
-      -- local unique_list, unique_block_list = u.unique_list_with_sync(unique_class, unique_class_rule_set)
-      local unique_list, unique_block_list = unique_class, unique_class_rule_set
-      for _, class in ipairs(unique_list) do
-        table.insert(classes, {
-          label = class,
-          kind = cmp.lsp.CompletionItemKind.Enum,
-          menu = file_name,
-          documentation = {
-            kind = 'markdown',
-            value = table.concat({
-              '```' .. 'css',
-              deindent(unique_block_list[_]),
-              '```'
-            }, '\n'),
-          }
-        })
+        -- local unique_list = u.unique_list(unique_class)
+        local unique_list, unique_block_list = u.unique_list_with_sync(unique_class, unique_class_rule_set)
+        -- local unique_list, unique_block_list = unique_class, unique_class_rule_set
+        for _, class in ipairs(unique_list) do
+          table.insert(classes, {
+            label = class,
+            kind = cmp.lsp.CompletionItemKind.Enum,
+            menu = file_name,
+            documentation = {
+              kind = 'markdown',
+              value = table.concat({
+                '```' .. 'css',
+                deindent(unique_block_list[_]),
+                '```'
+              }, '\n'),
+            }
+          })
+        end
+        -- local unique_ids_list = u.unique_list(unique_ids)
+        unique_list, unique_block_list = u.unique_list_with_sync(unique_ids, unique_ids_rule_set)
+        -- unique_list, unique_block_list = unique_ids, unique_ids_rule_set
+        for _, id in ipairs(unique_list) do
+          table.insert(ids, {
+            label = id,
+            kind = cmp.lsp.CompletionItemKind.Enum,
+            menu = file_name,
+            documentation = {
+              kind = 'markdown',
+              value = table.concat({
+                '```' .. 'css',
+                deindent(unique_block_list[_]),
+                '```'
+              }, '\n'),
+            }
+          })
+        end
       end
-      -- local unique_ids_list = u.unique_list(unique_ids)
-      -- unique_list, unique_block_list = u.unique_list_with_sync(unique_ids, unique_ids_rule_set)
-      unique_list, unique_block_list = unique_ids, unique_ids_rule_set
-      for _, id in ipairs(unique_list) do
-        table.insert(ids, {
-          label = id,
-          kind = cmp.lsp.CompletionItemKind.Enum,
-          menu = file_name,
-          documentation = {
-            kind = 'markdown',
-            value = table.concat({
-              '```' .. 'css',
-              deindent(unique_block_list[_]),
-              '```'
-            }, '\n'),
-          }
-        })
-      end
-    end
-    cb(classes, ids)
+      cb(classes, ids)
+    end)
   end
 end, 2)
 
